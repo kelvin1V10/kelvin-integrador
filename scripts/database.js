0b9fdbc8a8f5e685f6f89ad80c58030d7f797a97
kelvin1V10 <mtabrasil904@gmail.com>
2024-12-17 08:44:57 -0300

    sexo na laje

---
+++ scripts/database.js
@@ -0,0 +1,275 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto: 1,
+        tituloProduto: "PC Gamer Pichau Highflyer, Intel i7-14700K, GeForce RTX 4090 24GB, 32GB DDR5, SSD M.2 1TB",
+        preco: 5000,
+        descricao: "Pc gamer para rodar seus jogo com graficos caprichados e sem travamento.",
+        imagemProduto: {
+            img1: "./images/pc gamer(preto).jpg",
+            img2: "./images/pc gamer(preto1).jpg",
+            img3: "./images/pc gamer(preto2).jpg",
+            img4: "./images/pc gamer(preto3).jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 2,
+        tituloProduto: "PC Pichau Highflyer, AMD Ryzen 9 7950X, GeForce RTX 4090 24GB, 64GB DDR5, SSD M.2 2TB",
+        preco: 7000,
+        descricao: "1 x Processador AMD Ryzen 9 7950X, 16-Core, 32-Threads, 4.5GHz (5.7GHz Turbo), Cache 80, 1 x Water Cooler Corsair ICUE Link H170i LCD, 420mm, RGB, Preto, CW-9061009-WW, 1 x Placa Mae Asus Prime X670-P WIFI, DDR5, Socket AMD AM5, ATX, Chipset AMD X670, PR",
+        imagemProduto: {
+            img1: "./images/pc gamer(1).jpg",
+            img2: "./images/pc gamer(2).jpg",
+            img3: "./images/pc gamer(3).jpg",
+            img4: "./images/pc gamer(4).jpg",
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    
+    {
+        codigoProduto: 20,
+        tituloProduto: "Fonte Pichau Nidus, 1200W, Full Modular, 80 Plus Gold, Preto, PG-1201-BR",
+        preco: 360,
+        descricao: "Pronta para a nova geração de placas de vídeo! A fonte Pichau Nidus 1200W 80+ Gold conta com o padrão ATX3.1 e PCIE5.1, projetada com a utilização dos melhores componentes e aperfeiçoada para o mercado brasileiro com suas variações de energia 110V e 220V. Rigorosamente avaliada pelo laboratório CYBERNETICS que utiliza somente equipamentos topo de linha para os testes, recebendo as certificações Cybernetics ETA e Cybernetics LAMBDA A.",
+        imagemProduto: {
+            img1: "./images/fonte1.jpg",
+            img2: "./images/fonte2.jpg",
+            img3: "./images/fonte3.jpg",
+            img4: "./images/fonte4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 21,
+        tituloProduto: "Kit Ventoinhas Pichau Dual Ring P1, ARGB, 3x120mm, Preta, PG-DRP-BK01",
+        preco: 65,
+        descricao: "Precisando resfriar o seu setup? Conheça o kit ventoinhas Pichau Gaming Dual Ring P1 ARGB! Possuindo harmonia entre estilo, baixo ruído e ótimo desempenho, esta ventoinha é perfeita para qualquer configuração! Seu setup ficará muito mais responsivo depois desse upgrade, não te abandonará durante a jogatina e garantirá a performance desejada!",
+        imagemProduto: {
+            img1: "./images/cooler1.jpg",
+            img2: "./images/cooler2.jpg",
+            img3: "./images/cooler3.jpg",
+            img4: "./images/cooler4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 22,
+        tituloProduto: "Monitor Gamer Pichau Nexus Wide 34, 34 Pol, VA, Ultrawide, Curvo, 2K, 1ms, 165Hz, FreeSync, HDMI/DP, PG-NXSW34-BL01",
+        preco: 890,
+        descricao: "Monitor Gamer Pichau Nexus Wide 34, 34 Pol, VA, Ultrawide, Curvo, 2K, 1ms, 165Hz, FreeSync, HDMI/DP, PG-NXSW34-BL01",
+        imagemProduto: {
+            img1: "./images/monitor10.jpg",
+            img2: "./images/monitor11.jpg",
+            img3: "./images/monitor12.jpg",
+            img4: "./images/monitor13.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 23,
+        tituloProduto: "Cadeira Gamer TGT Heron TC, Espuma Moldada, Preto e Cinza, TGT-HRTC-BL02",
+        preco: 500,
+        descricao: "A TGT Heron TC é um produto ideal se você está procurando uma cadeira gamer com ótima qualidade e custo benefício Seu assento utiliza espuma moldada, e além disso, ela utiliza um pistão de alta durabilidade, tudo para que você se sinta confortável independente se vai passar horas jogando ou trabalhando.",
+        imagemProduto: {
+            img1: "./images/encosto1.jpg",
+            img2: "./images/encosto2.jpg",
+            img3: "./images/encosto3.jpg",
+            img4: "./images/encosto4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 24,
+        tituloProduto: "PC Gamer Pichau, AMD Ryzen 5 4600G, GeForce GTX 1060 6GB, 16GB DDR4, SSD 480GB",
+        preco: 2000,
+        descricao: "Conheça o Pichau PC Gamer, equipado com um processador AMD 4600G e 16GB de memória RAM. Este computador gamer entrega desempenho excepcional para jogos avançados e tarefas exigentes. Além disso, sua alta capacidade de memória RAM garante uma multitarefa suave e eficiente.",
+        imagemProduto: {
+            img1: "./images/rgb1.jpg",
+            img2: "./images/rgb2.jpg",
+            img3: "./images/rgb3.jpg",
+            img4: "./images/rgb4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 25,
+        tituloProduto: "Microfone Pichau Izar, RGB, USB, Preto, PG-IZR-RGB01",
+        preco: 210,
+        descricao: "Feito para gamers e criadores de conteúdo, a Pichau apresenta o microfone Izar! Agora suas lives e gravações terão toda a qualidade sonora que você e o seu público merecem. Além de agregar esteticamente ao seu setup, este microfone também traz as principais características que garantem a melhor qualidade de som, oferecendo qualidade de estúdio para sua casa!",
+        imagemProduto: {
+            img1: "./images/microfone1.jpg",
+            img2: "./images/microfone2.jpg",
+            img3: "./images/microfone3.jpg",
+            img4: "./images/microfone4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 26,
+        tituloProduto: "Cabo USB para teclado mecanico",
+        preco: 10,
+        descricao: "Cabo usb tipo c para teclado mecânico, cabo enrolado, teclado para jogos, aviador, fio de mola, computador de mesa, conector de aviação.",
+        imagemProduto: {
+            img1: "./images/cabo1.jpg",
+            img2: "./images/cabo2.jpg",
+            img3: "./images/cabo3.jpg",
+            img4: "./images/cabo4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Promocoes",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto: 28,
+        tituloProduto: "ASUS ProArt Display PA148CTV:",
+        preco: 660,
+        descricao: "Anunciado pela primeira vez na CES 2021, em janeiro passado, o novo monitor portátil ASUS ProArt Display PA148CTV para criadores de conteúdo já está disponível, ao preço de US$ 399. A tela possui Asus Dial e Painel de Controle Virtual para agilizar os processos criativos com atalhos e teclas de atalho programáveis, que aumentam a facilidade de uso com ferramentas criativas.",
+        imagemProduto: {
+            img1: "./images/monitor1.jpg",
+            img2: "./images/monitor2.jpg",
+            img3: "./images/monitor3.jog",
+            img4: "./images/monitor4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 6,
+        tituloProduto: "Cadeira gamer vermelha Pichau",
+        preco: 430,
+        descricao: "Cadeira vermelha Pichau boa para uso em trabalhos e jogos",
+        imagemProduto: {
+            img1: "./images/cadeira1.jpg",
+            img2: "./images/cadeira2.jpg",
+            img3: "./images/cadeira3.jpg",
+            img4: "./images/cadeira4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 7,
+        tituloProduto: "Fone de Ouvido Midnight Mancer Mcr-mnt-rgb01",
+        preco: 230,
+        descricao: "Fone de ouvido bom 7.1 de som bom para uso de jogos e para fins de trabalhos",
+        imagemProduto: {
+            img1: "./images/fone1.jpg",
+            img2: "./images/fone2.jpg",
+            img3: "./images/fone3.jpg",
+            img4: "./images/fone4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 8,
+        tituloProduto: "PCMK Hot Swap 60% JIS Linior Speed Silver - Branco [PK61W05BWJ] Layout japonês com fio 60% Tamanho Mecânico Silver Axis Gaming Keyboard Branco",
+        preco: 270,
+        descricao: " mecânico para jogos Teclas FN pré-programadas fáceis de usar 16,8,  para entrada duplicada Cabo,  USB-C para USB-A destacável Cabo paracord ultra-flexível e porta",
+        imagemProduto: {
+            img1: "./images/teclado1.jpg",
+            img2: "./images/teclado2.jpg",
+            img3: "./images/teclado3.jpg",
+            img4: "./images/teclado4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 9,
+        tituloProduto: "Placa de Video MSI GeForce RTX 4090 Gaming Trio, 24GB, GDDR6X, 384-bits, 912-V510-014",
+        preco: 12000,
+        descricao: "As placas de vídeo NVIDIA® GeForce RTX® Série 40 são mais que rápidas para gamers e criadores. Com a potente arquitetura NVIDIA Ada Lovelace, elas entregam um salto extraordinário em desempenho e gráficos alimentados por AI. Experimente mundos virtuais realistas com ray tracing e jogos com altíssimas taxas de FPS e a menor latência. Descubra novas maneiras revolucionárias para criar, além de uma aceleração de fluxos de trabalho sem precedentes.",
+        imagemProduto: {
+            img1: "./images/placa1.jpg",
+            img2: "./images/placa2.jpg",
+            img3: "./images/placa3.jpg",
+            img4: "./images/placa4.jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 10,
+        tituloProduto: "Xtreme PC Gaming MSI Geforce RTX 4060 TI Intel Core I9 14900KF 32GB DDR5 SSD 2TB Sistema Liquido WIFI Monster Hunter Edition Special",
+        preco: 10000,
+        descricao: "Intel Core i9 14900KF, Placa de vídeo NVIDIA GeForce RTX 4060 Ti 8GB GDDR6",
+        imagemProduto: {
+            img1: "./images/pc(controle).jpg",
+            img2: "./images/pc(controle1).jpg",
+            img3: "./images/pc(controle2).jpg",
+            img4: "./images/pc(controle4).jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 11,
+        tituloProduto: "Computador Pichau Gamer Afrodite, Intel i5-13600K, GeForce RTX 4070 Ti 12GB, 16GB DDR5, SSD M.2 1TB",
+        preco: 5000,
+        descricao: "undefined x Processador Intel Core i5-13600K, 14-Core, 20-Threads, 3.5GHz (5.1GHz Turbo), Cache 24MB, LGA1700, BX8071513600K, ",
+        imagemProduto: {
+            img1: "./images/pc(rgb1).jpg",
+            img2: "./images/pc(rgb2).jpg",
+            img3: "./images/pc(rgb3).jpg",
+            img4: "./images/pc(rgb4).jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 12,
+        tituloProduto: "PC Gamer Pichau Far Cry, AMD Ryzen 5 5500, GeForce RTX 3060 12GB, 16GB DDR4, SSD M.2 480GB",
+        preco: 9000,
+        descricao: " x Processador AMD Ryzen 5 5500, 6-Core, 12-Threads, 3.6GHz (4.2GHz Turbo), Cache 19M, 1 x Cooler Para Processador Gamdias Boreas E1-210 Lite, Raindow, 92mm, Preto, BOREAS-E",
+        imagemProduto: {
+            img1: "./images/pc gamer(22).jpg",
+            img2: "./images/pc gamer(100).jpg",
+            img3: "./images/pc gamer(300).jpg",
+            img4: "./images/pc(melhor).jpg",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+]
---
+++ scripts/orders.js
@@ -0,0 +1,110 @@
+import '../node_modules/xlsx/xlsx.js';
+ 
+// Get the orders from localStorage
+let orders = JSON.parse(localStorage.getItem("pedidos")) || []; // Default to empty array if no orders
+ 
+// Function to display the orders
+function displayOrders() {
+  if (orders && orders.length > 0) {
+    let orderList = document.getElementById("order-list");
+    let orderListHtml = "";
+ 
+    orders.forEach((order, index) => {
+      orderListHtml += `
+<li class="order-card">
+<h3>Pedido: ${order.id}</h3>
+<p>Endereço:</p>
+<ul>
+<li>Cliente: ${order.address.name} ${order.address.surname}</li>
+<li>Rua: ${order.address.address},${order.address.address_2}</li>
+<li>Cidade: ${order.address.city}</li>
+<li>UF: ${order.address.UF}</li>
+<li>CEP: ${order.address.zip}</li>
+</ul>
+<p>Itens:</p>
+<ul>
+          `;
+ 
+      // Check if order.items exists and is an array
+      if (order.items && Array.isArray(order.items)) {
+        order.items.forEach((item) => {
+          orderListHtml += `
+<li>
+<p>Codigo Produto: ${item.codigoProduto}</p>
+<p>Produto: ${item.tituloProduto}</p>
+<p>Quantidade: ${item.quantity}</p>
+<p>Preço: R$ ${item.preco}</p>
+</li>
+          `;
+        });
+      }
+ 
+      orderListHtml += `
+</ul>
+<p>Valor Total do Pedido: R$ ${order.totalValue}</p>
+</li>
+          `;
+    });
+ 
+    orderList.innerHTML = orderListHtml;
+  } else {
+    console.log("No orders found");
+  }
+}
+ 
+// Call the displayOrders function
+displayOrders();
+ 
+// Export function
+function exportToExcel() {
+  const pedidos = JSON.parse(localStorage.getItem("pedidos")) || []; // Default to empty array if no orders
+ 
+  const workbook = XLSX.utils.book_new();
+ 
+  const worksheet = XLSX.utils.aoa_to_sheet([
+    // Header row
+    ["ID", "E-mail", "Name", "Surname", "Phone", "CPF", "Address", "Zip", "Number", "Address 2", "Neighborhood", "City", "UF", "Item Code", "Item Title", "Item Price", "Item Description", "Item Category", "Item Classification", "Item Home Display", "Item Quantity", "Total Value"],
+    // Data rows
+    ...pedidos.flatMap((pedido) => {
+      if (pedido.items && Array.isArray(pedido.items)) {
+        return pedido.items.map((item) => [
+          pedido.id,
+          pedido.address.email,  // corrected "e-mail" to "email"
+          pedido.address.name,
+          pedido.address.surname,
+          pedido.address.phone,
+          pedido.address.CPF,
+          pedido.address.address,
+          pedido.address.zip,
+          pedido.address.number,
+          pedido.address.address_2,
+          pedido.address.neighborhood,
+          pedido.address.city,
+          pedido.address.UF,
+          item.codigoProduto,
+          item.tituloProduto,
+          item.preco,
+          item.descricao,
+          item.categoriaProduto,
+          item.classificacaoProduto,
+          item.exibirHome,
+          item.quantity,
+          pedido.totalValue,
+        ]);
+      }
+      return []; // Return empty array if no items
+    }),
+  ]);
+ 
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Orders");
+ 
+  XLSX.writeFile(workbook, "orders.xlsx");
+}
+ 
+// Add an event listener to the button
+const exportButton = document.getElementById("export-button");
+if (exportButton) {
+  exportButton.addEventListener("click", exportToExcel);
+} else {
+  console.log("Export button not found.");
+}
